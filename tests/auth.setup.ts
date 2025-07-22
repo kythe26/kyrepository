91b349878080b74ce41f6077002c92b8b1d494aa
Kythe Arguelles <kythe.g.arguelles@gmail.com>
2025-07-22 21:00:30 +0800

    auth-setup

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -3,11 +3,11 @@ import { expect, test as setup } from '@playwright/test';
 
 setup('Do Login', async ({ page }) => {
    
-    await page.goto('https://www.saucedemo.com/'); 
-    await page.locator('[data-test="username"]').fill(process.env.SAUCE_USERNAME!); 
+    await page.goto('https://www.saucedemo.com/');
+    await page.locator('[data-test="username"]').fill(process.env.SAUCE_USERNAME!);
     await page.locator('[data-test="password"]').fill(process.env.SAUCE_PASSWORD!); 
-    await page.locator('[data-test="login-button"]').click(); 
+    await page.locator('[data-test="login-button"]').click();
 
-    await expect(page.getByText('Swag Labs')).toBeVisible() 
+    await expect(page.getByText('Swag Labs')).toBeVisible()
     
-}); 
+});   
